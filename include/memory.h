57ef751de7c8420345ae7f32d81a45c21049354c
satish-pati <satishpati2006@gmail.com>
2025-02-16 16:59:02 +0530

    Update memory.h

--- include/memory.h
+++ include/memory.h
@@ -14,13 +14,11 @@ private:
 public:
     Memory(); // Constructor initializes memory to zero
     // Load word (4 bytes) from memory
-    uint32_t loadWord(uint32_t address) const;
-
+    uint32_t loadWord(uint32_t address,int coreID) const;
     // Store word (4 bytes) in memory
-    void storeWord(uint32_t address, uint32_t value);
-
+    void storeWord(uint32_t address, uint32_t value, int coreID);
     // Print memory content for debugging
-    void printMemory(uint32_t startAddress, uint32_t endAddress) const;
+    void printMemory( int coreID) const;
 };
 
 #endif // MEMORY_H
