13b5f27942bd8e1bfc62bdae71ccb80aa64513a1
niharika150107 <cs23b035@iittp.ac.in>
2025-02-17 01:48:22 +0530

    Updated core.h

--- include/core.h
+++ include/core.h
@@ -8,21 +8,19 @@
 class Core {
 private:
     Memory& memory;
-    uint32_t pc; // Program Counter
     int coreID;
     uint32_t baseAddress;
     bool isActive; // Flag to check if the core should continue execution
     //uint32_t registers[33] = {0};
-    std::vector<std::tuple<std::string, int, int, int, int>> &program ;// Reference to loaded instructions
-
+   std::vector<std::tuple<std::string, int, int, int, int,std::string>> &program ;// Reference to loaded instructions
 public:
-    Core(Memory& memRef, std::vector<std::tuple<std::string, int, int, int, int>>& program, int coreId); // Fix constructor declaration
-
+    uint32_t pc; // Program Counter
+    Core(Memory& memRef,std::vector<std::tuple<std::string, int, int, int, int,std::string>>& program, int coreId); // Fix constructor declaration
      uint32_t registers[33] = {0};
      
     uint32_t fetch(); // Fetch an instruction from memory
     void run(int numInstructions); // Run the core for a number of instructions
-    void execute(const std::string &instruction, int rd, int rs1, int rs2, int imm);
+    void execute( const std::string &instruction, int rd, int rs1, int rs2, int imm, const std::string &label, std::unordered_map<std::string, int> &labelMap); // Execute an instruction
 
 void printRegisters()const;
 
